0208d2fabdabf084acac3493e3a07e8b841512f7
Jeff Weimer <jweimer@weareenvoy.com>
2018-07-10 10:39:03 -0700

    netlify

--- netlify-functions/token.js
+++ netlify-functions/token.js
@@ -3,7 +3,18 @@ var admin = require('firebase-admin')
 var serviceAccount = require('./serviceAccountKey.json')
 
 var app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: 'service_account',
+    project_id: 'envoy-carwash',
+    private_key_id: `${process.env.PRIVATE_KEY_ID}`,
+    private_key: `${process.env.PRIVATE_KEY}`,
+    client_email: `${process.env.CLIENT_EMAIL}`,
+    client_id: `${process.env.CLIENT_ID}`,
+    auth_uri: `${process.env.AUTH_URI}`,
+    token_uri: `${process.env.TOKEN_URI}`,
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url: `${process.env.CLIENT_CERT}`
+  }),
   databaseURL: 'https://envoy-carwash.firebaseio.com'
 })
 
